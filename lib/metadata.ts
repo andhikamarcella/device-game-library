5e41843aa1edb6bb2d49858a13141b6586a97663
calla <andhikamarcellafernanda@gmail.com>
2025-11-22 16:53:32 +0700

    Handle missing involved companies for metadata extraction

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,13 +1,13 @@
-export function extractDevelopers(involved: any[]) {
-  return involved
-    ?.filter((i) => i?.developer)
+export function extractDevelopers(involved?: any[] | null) {
+  return (involved ?? [])
+    .filter((i) => i?.developer)
     .map((i) => i?.company?.name)
     .filter(Boolean);
 }
 
-export function extractPublishers(involved: any[]) {
-  return involved
-    ?.filter((i) => i?.publisher)
+export function extractPublishers(involved?: any[] | null) {
+  return (involved ?? [])
+    .filter((i) => i?.publisher)
     .map((i) => i?.company?.name)
     .filter(Boolean);
 }
